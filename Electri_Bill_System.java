66595c8fd38ced7ea0c51ffd3b88df41b81950a3
SaikiranPinnelli <95298804+SaikiranPinnelli@users.noreply.github.com>
2022-05-04 22:31:43 +0530

    Update Electri_Bill_System.java

--- Electri_Bill_System.java
+++ Electri_Bill_System.java
@@ -1,79 +1,34 @@
-package ElectricityBillSystem.Bill;
+package Electricity_Billing;
+import java.util.*;
 
-public class Electri_Bill_System extends Admin_Bill{
-
-	public static void main(String[] args)throws Exception {
-		System.out.println("Welcome to online Electricity Bill System!!!!!\n");
-		boolean rep=true;
-		while(rep) {
-		try {
-			System.out.println("Enter your credentials for login: ");
-			Credentials();
-			rep=false;
-			System.out.println("Successfully logged in ");
-		}
-			catch(Exception e) {
-				System.out.println("Wrong login credentials. Try Again !!!!!!!");
-				rep=true;
+public class ElectricityBillingSystem {
+	private static Scanner sc=new Scanner(System.in);
+	public static  String test() {
+		System.out.println("If u r an admin, type 'admin', if u r an user, type 'user'::");
+		String type=sc.next();
+		if(type.toLowerCase().equals("admin")==false && type.toLowerCase().equals("user")==false) {
+			while(type.toLowerCase().equals("admin")==false && type.toLowerCase().equals("user")==false) {
+				System.out.println("If u r an admin, type 'admin', if u r an use, type 'user'::");
+				type=sc.next();
 			}
 		}
-		   while(true) {
-			System.out.println("Enter your choice: ");
-			System.out.println("1: User 2: Admin 3: Exit ");
-			int choice=sc.nextInt();
-			switch(choice){
-			case 1:
-				System.out.println("1: Fetch Record 2: Bill_Paid 3: Exit");
-				int ch1=sc.nextInt();
-				switch(ch1) {
-				case 1:
-				fetchUserDetails();
-				break;
-				case 2:
-					Bill_Paid();
-					break;
-				case 3:
-					System.out.println("Exited Successfully");
-					System.exit(0);
-					break;
-					
-				default:
-					System.out.println("Invalid Choice");
-				}
-				break;
-			case 2:
-				System.out.println(" 1: Insert Record 2: Show Record 3: UpdateRecord 4: DeleteRecord 5: Exit");
-				int ch =sc.nextInt();
-				switch(ch) {
-			case 1:
-				InsertRecord();
-				break;
-				
-			case 2:
-				ShowRecord();
-				break;	
-			case 3:
-				UpdateRecord();
-				break;
-			case 4:
-				DeleteRecord();
-				break;
-			case 5:
-				System.out.println("Exited Successfully");
-				System.exit(0);
-				break;
-			default:
-				System.out.println("Invalid Choice");
-			}
-				break;
-			case 3:
-				System.out.println("Exited Successfully");
-				System.exit(0);
-				break;
-			default:
-				System.out.println("Invalid Choice");
-				
-		   }
+		return type;
 	}
-	}
+public static void main(String[] args) {
+ 
+	
+	String type=test();
+	
+	
+		if(type.toLowerCase().equals("admin")==true) {
+			Admin_Bill admin=new Admin_Bill();
+			admin.operations();
+			
+		}
+	else {
+		User user=new User();
+		user.operations();
+		}
+	
+}
 }
