9710657d7cc822c63bbb59570a87c359699e1611
SaikiranPinnelli <95298804+SaikiranPinnelli@users.noreply.github.com>
2022-05-05 21:54:58 +0530

    Update User.java

--- User.java
+++ User.java
@@ -18,19 +18,19 @@ public class User extends Details {
 		public void operations() {
 			try {
 			Class.forName("com.mysql.cj.jdbc.Driver"); //registering the driver class 1
-			url="jdbc:mysql://localhost:3306/fprojectONE"; // database path 3
+			url="jdbc:mysql://localhost:3306/db"; // database path 3
 			user="root";// 4
-			password="Qwertyuiop1!";// 5
+			password="Saikiran.123@";// 5
 			con=DriverManager.getConnection(url,user,password); 
-                       checkDetails();
-					
+                       String id=checkDetails();
+                       
 						System.out.println("If u wnt to see status of the payment, type'1'"
 								+ " and if u want to exit press any of 3,4,5,6,7,8 or 9::");
 						System.out.println("Insert choice:");
 						int choice=sc.nextInt();
 						switch(choice){
 						case 1:
-							check_Paid();
+							check_Paid(id);
 							break;
 						
 						
@@ -42,31 +42,31 @@ public class User extends Details {
 			catch(Exception e) {
 				e.printStackTrace();
 	}}
-		protected static void checkDetails() throws Exception {
+		protected static String checkDetails() throws Exception {
 		
-		String s1 ="select * from bill where cus_id = ?";
-			System.out.println("Fetching User details: ");
+		String s1 ="select * from user_register where cus_id = ?";
 			System.out.println("Enter your customer id: ");
 			PreparedStatement pre= con.prepareStatement(s1);
-			int id=sc.nextInt();
-	 		pre.setInt(1,id);
+			String id=sc.next();
+	 		pre.setString(1,id);
 			ResultSet rt=pre.executeQuery();
 			if(rt.next()) {
 				System.out.println("Successfully logged in");
+				return id;
 			}
 			else {
+				
 				System.out.println("First register yourself");
+				
 				System.exit(0);
 			}
+			return "";
 		}
-		protected static void check_Paid() throws Exception{
-			
+		protected static void check_Paid(String id) throws Exception{
 			
 			String s1 ="select * from bill where cus_id = ?";
-			System.out.println("Enter the customer id: ");
 			PreparedStatement pre= con.prepareStatement(s1);
-			int id=sc.nextInt();
-	 		pre.setInt(1,id);
+	 		pre.setString(1,id);
 	 		ResultSet rt=pre.executeQuery();
 	 		if(rt.next()) {
 	 			System.out.println("Bill not paid, your pending bill is: "+(rt.getInt(5)*500));
